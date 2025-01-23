7bdb355675067985c5061743a22886956b86dc5f
Haseem <haseem.khattak.hk@gmail.com>
2025-01-23 15:03:29 +0500

    improving command structured

--- cmd/serve.go
+++ cmd/serve.go
@@ -14,24 +14,26 @@ var folderPath string
 var serveCmd = &cobra.Command{
 	Use:   "upload",
 	Short: "Upload files from local machine to Google Drive",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		client, errResp := utils.GetDriveClient()
-		if errResp != nil {
-			return fmt.Errorf("unable to get HTTP client: %s", errResp.Error)
-		}
-		if folderPath == "" {
-			return errors.New("folder path is required")
-		}
-		err := drive.UploadFolder(client, folderPath)
-		if err != nil {
-			return fmt.Errorf("error uploading folder: %v", err)
-		}
-		return nil
-	},
+	RunE:  execute,
 }
 
 func init() {
 	serveCmd.Flags().StringVarP(&folderPath, "folder", "f", "", "Path to the local folder to upload")
 	serveCmd.MarkFlagRequired("folder")
 	rootCmd.AddCommand(serveCmd)
+}
+
+func execute(cmd *cobra.Command, args []string) error {
+	client, errResp := utils.GetDriveClient()
+	if errResp != nil {
+		return fmt.Errorf("unable to get HTTP client: %s", errResp.Error)
+	}
+	if folderPath == "" {
+		return errors.New("folder path is required")
+	}
+	err := drive.UploadFolder(client, folderPath)
+	if err != nil {
+		return fmt.Errorf("error uploading folder: %v", err)
+	}
+	return nil
 }
